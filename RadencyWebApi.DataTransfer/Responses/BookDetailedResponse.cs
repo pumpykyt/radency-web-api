d8bf2eba5e3d87365187fddf47100705a556f060
Andrew <37120451+pumpykyt@users.noreply.github.com>
2023-02-22 09:45:47 +0200

    refactor dto

--- RadencyWebApi.DataTransfer/Responses/BookDetailedResponse.cs
+++ RadencyWebApi.DataTransfer/Responses/BookDetailedResponse.cs
@@ -4,6 +4,9 @@ public class BookDetailedResponse
 {
     public int Id { get; set; }
     public string Title { get; set; }
+    public string Cover { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
     public decimal Rating { get; set; }
     public int ReviewsCount { get; set; }
     public List<ReviewResponse> Reviews { get; set; }
